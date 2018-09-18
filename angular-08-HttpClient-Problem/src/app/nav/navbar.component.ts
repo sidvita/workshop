cf00c03949033711e76d9652de7f2a992b759cdc
Chander Dhall <me@chanderdhall.com>
2018-09-18 17:37:41 -0500

    Problems added

---
+++ angular-08-HttpClient-Problem/src/app/nav/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../core/services/cart.service';
+import { NotificationMessage } from '../shared/models/notification-message.model';
+import { NotificationService } from '../core/services/notification.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  cartItemsTotal: number = 0;
+
+  constructor(private cartService: CartService, private notificationService: NotificationService) {}
+
+  ngOnInit() {
+    this.notificationService.notificationObservable.subscribe((message: NotificationMessage) => {
+      if (message.messageType === 'cartUpdated') {
+        this.cartItemsTotal = this.cartService.getCartQuantity();
+      }
+    });
+  }
+}
